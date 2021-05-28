ae3a0416350c1d1ba692f1968e8e10b2ff3f6099
lifi-h <jhak0001@student.monash.edu>
2021-05-28 12:05:29 +1000

    upload geographics

---
+++ geographics/assets/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    console.log("Hello world");
+  
+    (function ($) {
+      $.fn.clickToggle = function (func1, func2) {
+        var funcs = [func1, func2];
+        this.data("toggleclicked", 0);
+        this.click(function () {
+          var data = $(this).data();
+          var tc = data.toggleclicked;
+          $.proxy(funcs[tc], this)();
+          data.toggleclicked = (tc + 1) % 2;
+        });
+        return this;
+      };
+    })(jQuery);
+  
+    $(".thumbnail").clickToggle(
+      function () {
+        $(this).addClass("enlarge");
+      },
+      function () {
+        $(this).removeClass("enlarge");
+      }
+    );
+  });
+
+
+  $(document).ready(function () {
+    console.log("Hello world");
+  
+    $(".row").click(function () {
+      $(this).find(".content").slideToggle("fast");
+    });
+  });
+  
+  
